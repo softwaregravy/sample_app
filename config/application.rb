ec968b4abb83a9bb4ab04f8d7b3f50058183902f
John Hinnegan <john.hinnegan@gmail.com>
2010-10-25 20:47:47 -0400

    Revised and working with spork, autotest, rspec, and growl

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,11 @@ module SampleApp
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+
+    if Rails.env.test?
+      initializer :after => :initialize_dependecy_mechansism do
+        ActiveSupport::Dependencies.mechanism = :load 
+      end
+    end
   end
 end
