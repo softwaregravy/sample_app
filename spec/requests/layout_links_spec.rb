8df60e3bc37c9e0e3068132e80bf861e7c6220df
John Hinnegan <john.hinnegan@gmail.com>
2010-10-27 20:40:28 -0400

    Updated for end of CH 6 exercises

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -1,6 +1,20 @@
 require 'spec_helper'
 
+
 describe "LayoutLinks" do
+  def check_page_links(page)
+    visit page
+    check_link("About")
+    check_link("Help")
+    check_link("Contact")
+    check_link("Home")
+  end 
+
+  def check_link(link, content = link)
+    click_link link
+    response.should have_selector('title', :content => content)
+  end
+
   it "should have a Home page at '/'" do
     get '/'
     response.should have_selector('title', :content => "Home")
@@ -25,5 +39,23 @@ describe "LayoutLinks" do
     get '/signup'
     response.should have_selector('title', :content => "Sign up")
   end
+
+  it "should have the right links on the layout" do
+    check_page_links(about_path)
+  end
+
+  it "should have the right links on the layout" do
+    check_page_links(help_path)
+  end
+
+  it "should have the right links on the layout" do
+    check_page_links(contact_path)
+  end
+
+  it "should have the right links on the layout" do
+    check_page_links(root_path)
+    check_link("Sign up")
+  end
+
 
 end
